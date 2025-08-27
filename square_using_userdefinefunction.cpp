d0ad6c9ee6b9b53841086932fc1a950cf0fffc1e
naimul hasan nabil <naimulhasannabil760@gmail.com>
2025-08-27 22:33:16 +0600

    square

---
+++ square_using_userdefinefunction.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int square(int n)
+{
+    return n*n;
+}
+int main()
+{
+    int a;
+    cin>>a;
+    int result = square(a);
+    cout<<"Square is "<<result;
+
+
+
+}
