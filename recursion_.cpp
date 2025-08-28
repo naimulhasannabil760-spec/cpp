dc12a8c5b471811e00db7e9374c34b36fe6a906c
naimul hasan nabil <naimulhasannabil760@gmail.com>
2025-08-28 08:40:58 +0600

    recursion

---
+++ recursion_.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// Recursive function to calculate factorial
+int factorial(int n)
+{
+    if (n == 0 || n == 1) // Base case
+        return 1;
+    else
+        return n * factorial(n - 1); // Recursive call
+}
+
+int main()
+{
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num < 0)
+        cout << "Factorial is not defined for negative numbers." << endl;
+    else
+        cout << "Factorial of " << num << " = " << factorial(num) << endl;
+
+    return 0;
+}
