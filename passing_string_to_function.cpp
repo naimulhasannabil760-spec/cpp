62b6362873d5219d70b7874965740179e4fc8307
naimul hasan nabil <naimulhasannabil760@gmail.com>
2025-08-28 08:38:43 +0600

    string

---
+++ passing_string_to_function.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void displayString(string str)
+{
+    cout << "String: " << str << endl;
+}
+
+int main()
+{
+    string text = "Hello, Nabil!";
+    displayString(text);
+    return 0;
+}
